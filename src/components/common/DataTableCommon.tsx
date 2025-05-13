a157456f7def5f0931f88360669c998e0b6446f9
jbcPranta <jbc.pranta@gmail.com>
2025-05-13 14:49:15 +0600

    Table update

--- src/components/common/DataTableCommon.tsx
+++ src/components/common/DataTableCommon.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { IoMdArrowDropdown, IoMdArrowDropright } from "react-icons/io";
 import { Link } from "react-router-dom";
+import { CiSearch } from "react-icons/ci";
 
 type DataTableCommonProps = {
   tableHead: { text: string; key: string }[];
@@ -38,21 +39,7 @@ const DataTableCommon: React.FC<DataTableCommonProps> = ({ tableHead, data, head
 
         <div className="relative w-[30%]">
           <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-            <svg
-              className="w-4 h-4 text-gray-500 dark:text-gray-400"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 20 20"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-              />
-            </svg>
+            <CiSearch />
           </div>
           <input
             type="search"
